5b33b08219b584eda28c320f71bde58180e19b0b
Your NameFinal <final16700@gmail.com>
2021-09-28 09:14:36 +0530

    ...

--- src/Component/SubProductsOnNavBar.tsx
+++ src/Component/SubProductsOnNavBar.tsx
@@ -2,29 +2,35 @@ import React from 'react';
 import Popover from '@material-ui/core/Popover';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import { Grid, ListItem } from '@material-ui/core';
+import { Container, Grid, ListItem, MenuItem } from '@material-ui/core';
 import { subProductRoutes } from '../routes';
 import { NavLink } from 'react-router-dom';
+import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         popover: {
-            opacity: '95%'
+            opacity: '95%',
         },
         paper: {
-            padding: theme.spacing(1),
+            padding: theme.spacing(3),
+            width: 'fit-content'
         },
         wrapper: {
-            height: '100%',
-            width: '100%'
+            width: '100%',
+            height: 35,
+            marginBottom: 15,
+            '& svg':{
+                fontSize:12
+            }
         },
         pageLink: {
             width: '100%',
-            color:'#fff',
+            color: '#fff',
             backgroundColor: '#43a7dc',
             borderRadius: 4,
-            '& :hover':{
-                backgroundColor:'#43a7dc !important'
+            '&:hover': {
+                backgroundColor: '#43a7dc !important'
             }
         }
     }),
@@ -70,17 +76,23 @@ const SubProductsOnNavBar: React.FC = ({ children }: React.PropsWithChildren<{}>
                 onClose={handlePopoverClose}
                 disableRestoreFocus
             >
-                <Grid container spacing={2}>
-                    {subProductRoutes.map((route) =>
-                        <Grid item xs={4} key={route.text}>
-                            <Typography component='div' className={classes.wrapper}>
-                                <ListItem className={classes.pageLink} button to={route.link} component={NavLink} >
-                                    {route.text}
-                                </ListItem>
-                            </Typography>
-                        </Grid>
-                    )}
-                </Grid>
+                <Container fixed>
+                    <Grid container spacing={4}>
+                        {subProductRoutes.map((route) =>
+                            <Grid item xs={4} key={route.text}>
+                                <Typography component='div' className={classes.wrapper}>
+                                    <ListItem className={classes.pageLink} button to={route.link} component={NavLink} >
+                                        {route.text}
+                                    </ListItem>
+                                </Typography>
+                                {route.hashLink && route.hashLink.map((link) =>
+                                    <MenuItem key={link.text} >
+                                        <DoubleArrowIcon fontSize='small' />{link.text}</MenuItem>
+                                )}
+                            </Grid>
+                        )}
+                    </Grid>
+                </Container>
             </Popover>
         </React.Fragment>
     );
--- src/routes.ts
+++ src/routes.ts
@@ -14,16 +14,73 @@ import SubProduct from './Pages/SubProduct';
 export interface IRoute {
     text: string,
     link: string,
-    component: React.FC
+    component: React.FC,
+    hashLink?: { text: string, link: string }[]
 }
 
 export const subProductRoutes: IRoute[] = [
-    { text: 'Files and Folders', link: '/products/files-and-folders', component: SubProduct },
-    { text: 'Hospital Stationery', link: '/products/hospital-stationery', component: SubProduct },
-    { text: 'Identification Tags', link: '/products/identification-tags', component: SubProduct },
-    { text: 'Promotional Bags', link: '/products/promotional-bags', component: SubProduct },
-    { text: 'Office Stationery', link: '/products/office-stationery', component: SubProduct },
-    { text: 'Promotional Products', link: '/products/promotional-products', component: SubProduct }]
+    {
+        text: 'Files and Folders',
+        link: '/products/files-and-folders',
+        component: SubProduct,
+        hashLink: [
+            { text: 'Hospital File Folders', link: '#hospital-file-folders' },
+            { text: 'Doctor File Folders', link: '#doctor-file-folders' },
+            { text: 'PVC File Folder Files', link: '#pvc-file-folder-files' },
+            { text: 'Art Card and ITC File Folders', link: '#art-card-and-itc-file-folders' },
+            { text: 'Button File Folders', link: '#button-file-folders' }
+        ]
+    },
+    {
+        text: 'Hospital Stationery',
+        link: '/products/hospital-stationery',
+        component: SubProduct,
+        hashLink: [
+            { text: 'MRI and CT Scan Bags / Covers / Envelopes', link: '#mri-and-ct-scan-bags-covers-envelopes' },
+            { text: 'X- Ray Report Bags / X- Ray Envelopes', link: '#x-ray-report-bags-x-ray-envelopes' }
+        ]
+    },
+    {
+        text: 'Identification Tags',
+        link: '/products/identification-tags',
+        component: SubProduct,
+        hashLink: [
+            { text: 'Patient Identification Bands & Tags', link: '#patient-identification-bands-tags' },
+            { text: 'Identification Wristband', link: '#identification-wristband' }
+        ]
+    },
+    {
+        text: 'Promotional Bags',
+        link: '/products/promotional-bags',
+        component: SubProduct,
+        hashLink: [
+            { text: 'Gift Bags / Customise Bags', link: '#gift-bags-customise-bags' },
+            { text: 'Jewellery Bags', link: '#jewellery-bags' },
+            { text: 'Shopping Bags', link: '#shopping-bags' },
+            { text: 'My Clear Bag (Button Bag)', link: '#my-clear-bag-button-bag' }
+        ]
+    },
+    {
+        text: 'Office Stationery',
+        link: '/products/office-stationery',
+        component: SubProduct,
+        hashLink: [
+            { text: 'Educational Stationery', link: '#educational-stationery' },
+            { text: 'Innovative of Stationery', link: '#innovative-of-stationery' },
+            { text: 'Office Files & Display Files', link: '#office-files-display-files' }
+        ]
+    },
+    {
+        text: 'Promotional Products',
+        link: '/products/promotional-products',
+        component: SubProduct,
+        hashLink: [
+            { text: 'Printed CD"s & CD Covers', link: '#printed-cds-cd-covers' },
+            { text: 'Hand Fans / Promotional Hand Fans', link: '#hand-fans-promotional-hand-fans' },
+            { text: 'Danglers', link: '#danglers' }
+        ]
+    }
+]
 
 export const headerRoutes: IRoute[] = [
     { text: 'How it works', link: '/how-it-works', component: HowItWorks },
@@ -49,6 +106,6 @@ export const footerTop: IRoute[] = [
     { text: 'Blogs', link: '/blogs', component: Blogs },
     { text: 'FAQs', link: '/faqs', component: FAQs }]
 
-const routes: IRoute[] = [...headerRoutes, ...footerRoutes, ...homeRoutes, ...authRoutes,...subProductRoutes]
+const routes: IRoute[] = [...headerRoutes, ...footerRoutes, ...homeRoutes, ...authRoutes, ...subProductRoutes]
 
 export default routes;
